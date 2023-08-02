93a952c93953e2ac171b554e20f9d7ee90b38d1a
gns0314 <gns0314@naver.com>
2023-08-02 20:04:09 +0900

    import 수정

--- user/views.py
+++ user/views.py
@@ -4,7 +4,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from .models import User
 from .serializers import UserSerializer
 
 
